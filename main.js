62f488386bb69d5f9f932bbedb412b4da8effe9c
sotasan31033 <146412285+sotasan31033@users.noreply.github.com>
2024-06-04 01:11:45 +0900

    Firebase操作　テスト

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-console.log("Hello JavaScript");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDG9Pq3_aoeqn8cicipFOw9C10t2p3HD-o",
+  authDomain: "bosaiwebapp.firebaseapp.com",
+  projectId: "bosaiwebapp",
+  storageBucket: "bosaiwebapp.appspot.com",
+  messagingSenderId: "72888901207",
+  appId: "1:72888901207:web:c636d82631369489e80d12",
+  measurementId: "G-BK4W95VS7G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+
+/*
 //ブラウザの準備が出来たら実行する
 window.onload = (event)=> {
     console.log("OnLoad");
@@ -42,3 +68,4 @@ window.onload = (event)=> {
 
 
 }
+*/
