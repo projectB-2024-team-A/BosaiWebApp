3dbb9292316a02d3ea5892e3f2c20877cb467e50
c3p31084 <146412180+c3p31084@users.noreply.github.com>
2024-07-07 12:56:36 +0900

    向いている方向の正確性を検証

--- main2.js
+++ main2.js
@@ -128,7 +128,7 @@ window.onload = function(){
   //端末が対応していたら
   if (window.DeviceOrientationEvent) {
     //端末の方向が変わるたびに変更
-    window.addEventListener('deviceorientationabsolute', function(event) {
+    window.addEventListener('deviceorientation', function(event) {
       var nowHeading = event.alpha;
       if(i===0){
         alert(nowHeading);
